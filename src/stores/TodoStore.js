2e7b7917d9eb0efc8d8aaf73c29d010895561a81
David Gómez <ldavid.github@gmail.com>
2016-08-22 10:11:23 +0200

    added create and update functions to store

--- src/stores/TodoStore.js
+++ src/stores/TodoStore.js
@@ -9,3 +9,33 @@ var assign = require('object-assign');
 var CHANGE_EVENT = 'change';
 
 var _todos = {};
+
+var create = function (text) {
+    var id = (+new Date() + Math.floor(Math.random() * 999999)).toString(36);
+    _todos[id] = {
+        id: id,
+        completed: false,
+        text: text
+    };
+};
+
+var update = function (id, updates) {
+    _todos[id] = assign({}, _todos[id], updates);
+};
+
+var TodoStore = assign({}, EventEmitter.prototype, {
+    getAll: function () {
+        return _todos;
+    },
+    emitChange: function () {
+        this.emit(CHANGE_EVENT);
+    },
+
+    addChangeListener: function (callback) {
+        this.on(CHANGE_EVENT, callback);
+    },
+    removeChangeListener: function (callback) {
+        this.on(CHANGE_EVENT, callback)
+    }
+});
+});
