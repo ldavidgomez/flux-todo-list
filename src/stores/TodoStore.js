67c5b3c3a2223079cce302754b73084eca8afbd9
David Gómez <ldavid.github@gmail.com>
2016-08-22 10:08:29 +0200

    added store

---
+++ src/stores/TodoStore.js
@@ -0,0 +1,11 @@
+/**
+ * Created by David on 22/08/2016.
+ */
+var AppDispatcher = require('../dispatcher/AppDispatcher');
+var EventEmitter = require('events').EventEmitter;
+var TodoConstants = require('../constants/TodoConstants');
+var assign = require('object-assign');
+
+var CHANGE_EVENT = 'change';
+
+var _todos = {};
